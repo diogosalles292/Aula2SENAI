71b80e57678e57958462c273acc7781cd7b2677e
diogosalles292 <144277823+diogosalles292@users.noreply.github.com>
2023-09-13 16:51:55 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+numero = int(input("digite um numero"))
+numero2 = int(input("digite outro numero"))
+print(numero + numero2)
+
+nome = "Diogo"
+print(nome)
+
+nome = "Diogo"
+cumprimento = "Ola"
+print(comprimento, nome)
+
+numero4 = int(input("digite um numero"))
+numero5 = int(input("digite outro numero"))
+print(numero4* numero5)
